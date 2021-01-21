81ca388d455d65323b2e5c6fdf16b7a67286c421
Carmen <carmen@oursky.com>
2021-01-21 15:58:52 +1100

    Update post build script

--- example/reactnative/appcenter-post-clone.sh
+++ example/reactnative/appcenter-post-clone.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# install dependencies for React Native SDK
-cd ../../packages/authgear-react-native
-npm install
-popd
-# install dependencies for Web SDK
-cd ../../packages/authgear-web
-npm install
-popd
-# to SDK project root
-cd ../..
-# install dependencies
-npm install
-# build authgear JS SDK
+npm ci
+npm run lerna bootstrap
 npm run build
